24481bf16624606738b7c19679e62c1c74b650ef
Sherin Olivia <SOChronicle@gmail.com>
2023-07-11 06:15:09 +0700

    created the introductory section of main

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const navPages = document.getElementsByClassName('nav-pages')[0]
 hamburgerBtn.addEventListener('click', function(){
     navPages.classList.toggle('d-none')
 })
+
+let goTopBtn = document.getElementById("toTopBtn");
+
+window.onscroll = function() {
+    showButtonFunction()
+};
+
+function showButtonFunction() {
+    if (document.documentElement.scrollTop > 20) {
+        goTopBtn.style.display = "block";
+      } else {
+        goTopBtn.style.display = "none";
+      }
+}
+
+function toTopFunction() {
+    document.documentElement.scrollTop = 0;
+}
