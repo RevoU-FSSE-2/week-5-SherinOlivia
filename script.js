2951cd56a8d579538d1a0f81b74ea9174ed57c27
Sherin Olivia <SOChronicle@gmail.com>
2023-07-13 05:57:29 +0700

    worked on footer 70% progress

--- script.js
+++ script.js
@@ -6,20 +6,41 @@ hamburgerBtn.addEventListener('click', function(){
     navPages.classList.toggle('d-none')
 })
 
-let goTopBtn = document.getElementById("toTopBtn");
 
-window.onscroll = function() {
-    showButtonFunction()
-};
 
-function showButtonFunction() {
-    if (document.documentElement.scrollTop > 20) {
-        goTopBtn.style.display = "block";
-      } else {
-        goTopBtn.style.display = "none";
-      }
+// Cookie Modal:
+let cookieModal = document.getElementById("cookieModal");
+let openCookie = document.getElementById("cookieDeclaration");
+let closeCookie = document.getElementsByClassName("close-cookie-modal")[0];
+
+openCookie.onclick = function() {
+    cookieModal.style.display = "block";
 }
 
-function toTopFunction() {
-    document.documentElement.scrollTop = 0;
+closeCookie.onclick = function() {
+    cookieModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == cookieModal) {
+    cookieModal.style.display = "none";
+  }
+}
+
+let catchphraseModal = document.getElementById("catchphraseModal");
+let openCatchphrase = document.getElementById("catchphraseLogo");
+let closeCatchphrase = document.getElementsByClassName("close-catchphrase-modal")[0];
+
+openCatchphrase.onclick = function() {
+    catchphraseModal.style.display = "block";
+}
+
+closeCatchphrase.onclick = function() {
+    catchphraseModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == catchphraseModal) {
+    catchphraseModal.style.display = "none";
+  }
 }
